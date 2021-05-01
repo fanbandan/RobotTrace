7fa926a2cf59d2bbf3c6b7399719a2fa517feb4e
= <=>
2021-05-01 11:29:35 +1000

    Interface boilerplate

---
+++ src/+ComputerVision/Interface.m
@@ -0,0 +1,12 @@
+classdef Interface < handle
+    %Vision Interface - Interface for recognising game path in camera image feed.
+    %   DETAILED DESCRIPTION GOES HERE
+    properties (Access = public)
+    end
+    properties (Access = private)
+    end
+    methods (Access = public)
+    end
+    methods (Access = private)
+    end
+end
