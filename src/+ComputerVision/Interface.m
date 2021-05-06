19d0025a244af74b483c679ea9f67eaafeeb8c14
himavaan1 <himavaan.chandra@student.uts.edu.au>
2021-05-07 00:06:55 +1000

    added vision interface

--- src/+ComputerVision/Interface.m
+++ src/+ComputerVision/Interface.m
@@ -8,10 +8,14 @@ classdef Interface < handle
     methods (Access = public)
         function self = Interface()
         end
-        function [M,u,v] = GetPathPixel(self)
+        function [M,u,v] = GetPathPixel(self, image)
             %GetPathPixel returns a mask of pixels containing copper pipe.
             %   [1] pixel at uv coordinate contains copper pipe.
             %   [0] pixel at uv coordinate does not contain copper pipe.
+            vision = Vision;
+            maskedRGBImage = vision.colourMask(image);
+            edgeImage = vision.edgeDetection(maskedRGBImage);
+            [M,u,v] = edgeImage;
         end
     end
     methods (Access = public)
