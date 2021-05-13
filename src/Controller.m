b78c032c80c69cbb0691621b48a606b3d710200f
= <=>
2021-05-13 23:37:37 +1000

    non ros testing

--- src/Controller.m
+++ src/Controller.m
@@ -8,19 +8,27 @@ classdef Controller < handle
         CVI ComputerVision.Interface
         PEI PathExtraction.Interface
         RMRCI RMRC.Interface
+        rosMode logical = true;
         debug logical = false;
         
         imageMask;
         pathPoints;
+        pathStartGuess;
     end
     methods (Access = public)
-        function self = Controller(debug, rosMode)
+        function self = Controller(rosMode, debug)
             %CONTROLLER Construct an instance of this class
             %   Detailed explanation goes here
-            self.debug = debug;
-            self.CVI = ComputerVision.Interface(debug, false);
-            self.PEI = PathExtraction.Interface(debug);
-            self.RMRCI = RMRC.Interface(self.CVI, debug, rosMode);
+            deltaT = 0.2;
+            if exist('debug','var')
+                self.debug = debug;
+            end
+            if exist('rosMode','var')
+                self.rosMode = rosMode;
+            end
+            self.CVI = ComputerVision.Interface(self.rosMode, self.debug);
+            self.PEI = PathExtraction.Interface(self.debug);
+            self.RMRCI = RMRC.Interface(self.CVI, deltaT, self.rosMode, self.debug);
         end
         function image = AcquireImageMask(self)
             %METHOD1 Summary of this method goes here
@@ -41,17 +49,30 @@ classdef Controller < handle
             else
                 [normal, point] = self.CVI.GetGamePlane();
             end
+            if self.debug == true
+                figure(104);
+            end
             self.PEI.UpdatePathMask(self.imageMask, cameraMatrix, normal, point);
         end
         function GeneratePath(self, downsample, maxDistance, averaging, smoothing)
-            startGuess = self.RMRCI.GetRobotPose();
-            startGuess = [0.306,0.28,0.60];
+            startGuess = self.pathStartGuess;
+            if self.debug == true
+                figure(105);
+            end
             path = self.PEI.GeneratePath(downsample, startGuess, maxDistance);
+            
             if length(path) < 5
                 warning('No path found!');
                 return
             end
             self.PEI.GenerateSpline(averaging, smoothing);
+        end
+        function UpdatePathStartGuess(self, manualGuess)
+            if exist('manualGuess','var')
+                self.pathStartGuess = manualGuess;
+            else
+                self.pathStartGuess = self.RMRCI.GetRobotPose();
+            end
         end
         function ShowPath(self)
             self.PEI.PlotSpline(figure(402));
@@ -72,6 +93,15 @@ classdef Controller < handle
         end
         function EStop(self)
             self.RMRCI.Stop();
+        end
+    end
+    methods
+        % Debug / Visual methods
+        function ShowPathPointCloud(self, h)
+            self.PEI.ShowPointCloud(h);
+        end
+        function ShowPathSpline(self, h)
+            self.PEI.PlotSpline(h);
         end
     end
     methods (Access = protected)
