6f2cee8e41fbca327eeb959f051728895a282100
HimavaanChandra <himavaan.chandra@student.uts.edu.au>
2021-05-13 18:43:49 +1000

    Debugging with demo image

--- src/+ComputerVision/@Vision/Vision.m
+++ src/+ComputerVision/@Vision/Vision.m
@@ -27,19 +27,19 @@ classdef Vision < handle
             I = rgb2hsv(image);
             
             % Define thresholds for channel 1 based on histogram settings
-            channel1Min = 0.940;
-            channel1Max = 0.170;
+            channel1Min = 0.000;
+            channel1Max = 0.160;
             
             % Define thresholds for channel 2 based on histogram settings
-            channel2Min = 0.210;
+            channel2Min = 0.050;
             channel2Max = 1.000;
             
             % Define thresholds for channel 3 based on histogram settings
-            channel3Min = 0.000;
+            channel3Min = 0.726;
             channel3Max = 1.000;
             
             % Create mask based on chosen histogram thresholds
-            sliderBW = ( (I(:,:,1) >= channel1Min) | (I(:,:,1) <= channel1Max) ) & ...
+            sliderBW = (I(:,:,1) >= channel1Min ) & (I(:,:,1) <= channel1Max) & ...
                 (I(:,:,2) >= channel2Min ) & (I(:,:,2) <= channel2Max) & ...
                 (I(:,:,3) >= channel3Min ) & (I(:,:,3) <= channel3Max);
             BW = sliderBW;
