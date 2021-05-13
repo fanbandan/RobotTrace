31ddb748220a2c5393d5648972517232fb1dd5f5
HimavaanChandra <himavaan.chandra@student.uts.edu.au>
2021-05-13 15:41:10 +1000

    updated transforms

--- src/+ComputerVision/Interface.m
+++ src/+ComputerVision/Interface.m
@@ -7,7 +7,7 @@ classdef Interface < handle
         vision Vision
         ar AR
         Debug logical = false;
-        ROSMode logical = true;
+        rosMode logical = true;
         gameTag
         gameRotm
         robotTag
@@ -19,9 +19,9 @@ classdef Interface < handle
                 self.Debug = debug;
             end
             if ~isempty(rosMode)
-                self.ROSMode = rosMode;
+                self.rosMode = rosMode;
             end
-            if self.ROSMode == false
+            if self.rosMode == false
                 self.vision = ComputerVision.Vision(debug);
                 self.ar = ComputerVision.AR(debug);
             end
@@ -55,63 +55,22 @@ classdef Interface < handle
         end
     end
     methods (Access = public)
-        %Transformation Matrices
-        function SetTags(self, gameTag, robotTag)
-            self.gameTag = gameTag;
-            self.robotTag = robotTag;
-            self.gameRotm = quat2rotm([self.gameTag.pose.orientation.x self.gameTag.pose.orientation.y self.gameTag.pose.orientation.z self.gameTag.pose.orientation.w]);
-            self.robotRotm = quat2rotm([self.robotTag.pose.orientation.x self.robotTag.pose.orientation.y self.robotTag.pose.orientation.z self.robotTag.pose.orientation.w]);
-        end
-        function [gameTag , robotTag] = GetTags(self)
-            gameTag = self.gameTag;
-            robotTag = self.robotTag;
-        end
-        %Transformation matrix
-        % T= [r11 r12 r13 px;
-        %     r21 r22 r23 py;
-        %     r31 r32 r33 pz;
-        %     0    0    0  1;];
-        %   Derived from AR Tags - Remember tag coordinate system is different (z is depth)
-        function T = GetRobot2GameTransformationMatrix(self)
-            rotm = self.gameRotm - self.robotRotm;
-            rotm = [rotm; zeros(1,3)];
-            transm = [self.gameTag.pose.position.x - self.robotTag.pose.position.x; self.gameTag.pose.position.y - self.robotTag.pose.position.y; self.gameTag.pose.position.z - self.robotTag.pose.position.z;1;];
-            T= [rotm transm];
-        end
-        function T = GetGame2RobotTransformationMatrix(self)
-            rotm = self.robotRotm - self.gameRotm;
-            rotm = [rotm; zeros(1,3)];
-            transm = [self.robotTag.pose.position.x - self.gameTag.pose.position.x; self.robotTag.pose.position.y - self.gameTag.pose.position.y; self.robotTag.pose.position.z - self.gameTag.pose.position.z;1;];
-            T= [rotm transm];
-        end
         function T = GetCamera2GameTransformationMatrix(self)
-            rotm = self.gameRotm;
-            rotm = [rotm; zeros(1,3)];
-            transm = [self.gameTag.pose.position.x; self.gameTag.pose.position.y; self.gameTag.pose.position.z;1;];
-            T= [rotm transm];
+            T = self.ar.GetGamePose();
         end
         function T = GetGame2CameraTransformationMatrix(self)
-            rotm = -self.gameRotm;
-            rotm = [rotm; zeros(1,3)];
-            transm = [-self.gameTag.pose.position.x; -self.gameTag.pose.position.y; -self.gameTag.pose.position.z;1;];
-            T= [rotm transm];
+            T = inv(self.ar.GetGamePose());
         end
         function T = GetRobot2CameraTransformationMatrix(self)
-            rotm =  -self.robotRotm;
-            rotm = [rotm; zeros(1,3)];
-            transm = [-self.robotTag.pose.position.x; -self.robotTag.pose.position.y; -self.robotTag.pose.position.z;1;];
-            T= [rotm transm];
+            T = inv(self.ar.GetRobotPose());
         end
         function T = GetCamera2RobotTransformationMatrix(self)
-            rotm = self.robotRotm;
-            rotm = [rotm; zeros(1,3)];
-            transm = [self.robotTag.pose.position.x; self.robotTag.pose.position.y; self.robotTag.pose.position.z;1;];
-            T= [rotm transm];
+            T = self.ar.GetRobotPose();
         end
     end
     methods (Access = private)
         function image = GetImage(self)
-            if self.ROSMode == true
+            if self.rosMode == true
                 image = self.vision.getImage();
             else
                 % Change this to some default image
