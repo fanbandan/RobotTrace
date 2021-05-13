208578820c927776e3bf77164aa2236f0b284547
Chloe-Elise <chloe.mallinson@student.uts.edu.au>
2021-05-13 10:14:54 +1000

    Pseudocode of light curtain and safety retreat. Also addition of skeleton jogging with XBox controller code

---
+++ test/LightCurtain.m
@@ -0,0 +1,19 @@
+%% Safety Light Curtain Restraints
+
+% Intersection bool
+% Number of planes
+% Empty traj point int
+
+% Create a plan at all 6 walls of the workspace cube by obtaining the
+% workspace dimensions
+
+% Get the points within the current trajectory and number of points
+
+% Check for intersection of points with the planes
+    % for i = (1: points)
+        % for j = (1: planes)
+            % if int = true
+                % stop
+                % break
+        % end
+    % end
---
+++ test/SafetyRetreat.m
@@ -0,0 +1,11 @@
+%% For One AR Tag
+
+% Get AR Pose array (in 3D coords)
+
+% Get current EE pose (3D)
+
+% Calculate pose some distance from the AR tag (T)
+
+% Use robot ikine to find joint pose q
+
+% Move to joint configuration (robot jtraj)
