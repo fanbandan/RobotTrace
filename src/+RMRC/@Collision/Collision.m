51fddb096aff29d9d672c8c1241572005b7d7a83
= <=>
2021-05-15 11:31:50 +1000

    collision scaffold

---
+++ src/+RMRC/@Collision/Collision.m
@@ -0,0 +1,13 @@
+classdef Collision < handle
+    %Collision Generic class
+    %   Provides generic functionality robot collision detection.
+    properties
+    end
+    methods
+        function self = Collision()
+        end
+        function delete(self)
+        end
+        
+    end
+end
