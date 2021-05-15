b49d32aaa6fa6b21e2f9c2ed327e2473d0f9ffd7
fanbandan <52903531+fanbandan@users.noreply.github.com>
2021-05-15 23:02:10 +1000

    implemented ellipsoid collision

--- src/+RMRC/@Collision/Collision.m
+++ src/+RMRC/@Collision/Collision.m
@@ -2,72 +2,90 @@ classdef Collision < handle
     %Collision Generic class
     %   Provides generic functionality robot collision detection.
     properties
-        qMatrix;
+        robot;
+        collisionPoints;
     end
     methods
         % create ellipses between links
         % fkine to get joint position
         % check collisions between ellipses
-        function self = Collision()
-            [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
-            ellipsoidAtOrigin_h = surf(X,Y,Z);
+        function self = Collision(robot)
+            self.robot = robot;
+        end
+        function CheckCollision(self, q)
             
-            %             % 2.9
-            %             q = [0,0,0]
-            %             tr = robot.fkine(q);
-            %             cubePointsAndOnes = [inv(tr) * [cubePoints,ones(size(cubePoints,1),1)]']';
-            %             updatedCubePoints = cubePointsAndOnes(:,1:3);
-            %             algebraicDist = GetAlgebraicDist(updatedCubePoints, centerPoint, radii);
-            %             pointsInside = find(algebraicDist < 1);
-            %             display(['2.9: There are now ', num2str(size(pointsInside,1)),' points inside']);
-            %
-            %             % 2.10
-            %             q = [0,0,0]
-            %             tr = zeros(4,4,robot.n+1);
-            %             tr(:,:,1) = robot.base;
-            %             L = robot.links;
-            %             for i = 1 : robot.n
-            %                 tr(:,:,i+1) = tr(:,:,i) * trotz(q(i)) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
-            %             end
+            %%
+            [x(1),y(1),z(1)] = self.robot.LinkKinematics(q(1:1));
+            [x(2),y(2),z(2)] = self.robot.LinkKinematics(q(1:2));
+            [x(3),y(3),z(3)] = self.robot.LinkKinematics(q(1:3));
+            [x(4),y(4),z(4)] = self.robot.LinkKinematics(q(1:4));
+            [x(5),y(5),z(5)] = self.robot.LinkKinematics(q(1:5));
             
-            %             % Go through each ellipsoid
-            %             for i = 1: size(tr,3)
-            %                 cubePointsAndOnes = [inv(tr(:,:,i)) * [cubePoints,ones(size(cubePoints,1),1)]']';
-            %                 updatedCubePoints = cubePointsAndOnes(:,1:3);
-            %                 algebraicDist = GetAlgebraicDist(updatedCubePoints, centerPoint, radii);
-            %                 pointsInside = find(algebraicDist < 1);
-            %                 display(['2.10: There are ', num2str(size(pointsInside,1)),' points inside the ',num2str(i),'th ellipsoid']);
-            %             end
+            figure(1);
+            clf;
+            self.robot.PlotRobot([-1,1,-1,1,-1,1],q);
             
-            [x1,y1,z1] = LinkKinematics(qMatrix(1));
-            [x2,y2,z2] = LinkKinematics(qMatrix(2));
-            [x3,y3,z3] = LinkKinematics(qMatrix(3));
-            [x4,y4,z4] = LinkKinematics(qMatrix(4));
-            [x5,y5,z5] = LinkKinematics(qMatrix(5));
+            H = hgtransform('Parent',gca);
+            hold on;
+            cube_h = plot3(self.collisionPoints(:,1),self.collisionPoints(:,2),self.collisionPoints(:,3),'b.');
             
-            baseToq2 = ([x2,y2,z2] + [x1,y1,z1])/2;
-            q2Toq3 = ([x3,y3,z3] + [x2,y2,z2])/2;
-            q3Toq4 = ([x4,y4,z4] + [x3,y3,z3])/2;
-            q4Toq5 = ([x5,y5,z5] + [x4,y4,z4])/2;
-            radii = [1 2 3];
-            %             [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3));
-            [X,Y,Z] = ellipsoid( q4Toq5(1), q4Toq5(2), q4Toq5(3), radii(1), radii(2), radii(3));
-            ellipsoidAtOrigin_h = surf(X,Y,Z);
-            % Need to check collisions with table and itself---------------
-            %             Go through each ellipsoid
-            for i = 1: size(tr,3)
-                cubePointsAndOnes = [inv(tr(:,:,i)) * [cubePoints,ones(size(cubePoints,1),1)]']';
-                updatedCubePoints = cubePointsAndOnes(:,1:3);
-                algebraicDist = GetAlgebraicDist(updatedCubePoints, centerPoint, radii);
+            tr = zeros(4,4,3);
+            radii = [0.1,0.025,0.025];
+            
+            for i = 1:3
+                position = ([x(i+1),y(i+1),z(i+1)] + [x(i),y(i),z(i)])/2;
+                dx = x(i+1)-x(i);
+                dy = y(i+1)-y(i);
+                dz = z(i+1)-z(i);
+                L = sqrt(dx^2+dy^2);
+                angleZ = atan2(dy,dx);
+                angleY = atan2(dz,L);
+                T = transl(position(1),position(2),position(3))*trotz(angleZ)*troty(-angleY);
+%                 trplot(T);
+                tr(:,:,i) = T;
+                [X,Y,Z] = ellipsoid(0,0,0,radii(1),radii(2),radii(3));
+                ellipsoidAtOrigin_h = surf(X,Y,Z);
+                set(ellipsoidAtOrigin_h,'Parent',H);
+                set(H,'Matrix',T);
+                pause;
+                cubePointsAndOnes = [inv(tr(:,:,i)) * [self.collisionPoints,ones(size(self.collisionPoints,1),1)]']';
+                transformedPoints = cubePointsAndOnes(:,1:3);
+                algebraicDist = self.GetAlgebraicDist(transformedPoints, position, radii);
                 pointsInside = find(algebraicDist < 1);
-                display(['2.10: There are ', num2str(size(pointsInside,1)),' points inside the ',num2str(i),'th ellipsoid']);
+                disp(['2.10: There are ', num2str(size(pointsInside,1)),' points inside the ',num2str(i),'th ellipsoid']);
             end
+        end
+        function GenerateCube(self, x, y, z)
+            %% One side of the cube
+            [Y,Z] = meshgrid(-0.25:0.005:0.25,-0.25:0.005:0.25);
+            sizeMat = size(Y);
+            X = repmat(0.25,sizeMat(1),sizeMat(2));
             
+            % Combine one surface as a point cloud
+            cubePoints = [X(:),Y(:),Z(:)];
+            
+            % Make a cube by rotating the single side by 0,90,180,270, and around y to make the top and bottom faces
+            cubePoints = [ cubePoints ...
+                ; cubePoints * rotz(pi/2)...
+                ; cubePoints * rotz(pi) ...
+                ; cubePoints * rotz(3*pi/2) ...
+                ; cubePoints * roty(pi/2) ...
+                ; cubePoints * roty(-pi/2)];
+            hold on;
+            cubeAtOigin_h = plot3(cubePoints(:,1),cubePoints(:,2),cubePoints(:,3),'r.');
+            cubePoints = cubePoints + repmat([x,y,z],size(cubePoints,1),1);
+            cube_h = plot3(cubePoints(:,1),cubePoints(:,2),cubePoints(:,3),'b.');
+            self.collisionPoints = cubePoints;
         end
-        function setQMatrix(self, qMatrix)
-            self.qMatrix = qMatrix;
-        end
-        function algebraicDist = GetAlgebraicDist(points, centerPoint, radii)
+        function algebraicDist = GetAlgebraicDist(self, points, centerPoint, radii)
+            
+            figure(2);
+            clf;
+            hold on
+            cube_h = plot3(points(:,1),points(:,2),points(:,3),'b.');
+            
+            [X,Y,Z] = ellipsoid(centerPoint(1),centerPoint(2),centerPoint(3),radii(1),radii(2),radii(3));
+            ellipsoidAtOrigin_h = surf(X,Y,Z);
             
             algebraicDist = ((points(:,1)-centerPoint(1))/radii(1)).^2 ...
                 + ((points(:,2)-centerPoint(2))/radii(2)).^2 ...
