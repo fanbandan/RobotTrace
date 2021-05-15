83618ca02c9421b38def125817264ffcae6ca03d
himavaan1 <himavaan.chandra@student.uts.edu.au>
2021-05-15 15:17:45 +1000

    collision stuff

--- src/+RMRC/@Collision/Collision.m
+++ src/+RMRC/@Collision/Collision.m
@@ -3,13 +3,22 @@ classdef Collision < handle
     %   Provides generic functionality robot collision detection.
     properties
         qMatrix;
+        base;
     end
     methods
         % create ellipses between links
         % fkine to get joint position
         % check collisions between ellipses
-        function self = Collision()
-            radii = [3,2,1];
+        function self = Collision(base)
+%             radii = [3,2,1];
+            radii = [0.147,0.01,0.01];
+            self.base = base;
+            %q1 is linear rail therefore not used
+            q2 = [self.base dobot.fkine(qMatrix[1])];
+            q3 = [dobot.fkine(qMatrix[1]) dobot.fkine(qMatrix[1])];
+            q4 = [dobot.fkine(qMatrix[1]) dobot.fkine(qMatrix[1])];
+            q5 = [dobot.fkine(qMatrix[1]) dobot.fkine(qMatrix[1])]
+            
             [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
         end
         function setQMatrix(self, qMatrix)
